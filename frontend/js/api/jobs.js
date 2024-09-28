cf01817a7991a62cb51f64ed1e0e272e0b9a580b
Hyeonseo <tym30003@gmail.com>
2024-09-28 14:24:03 -0600

    Update 2

--- frontend/js/api/jobs.js
+++ frontend/js/api/jobs.js
@@ -1,4 +1,13 @@
 // your code goes here.
 
-const BASE_URL = "http://localhost:3000/jobs"
+const BASE_URL = "http://localhost:3000"
 
+const getJobList=(url=`${BASE_URL}/jobs/`)=>{
+    return fetch(url).then((response)=>{
+        return response.json();
+    }).then((data)=>{
+        return data;
+    })
+}
+
+export {getJobList}
--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,4 +1,8 @@
 // your code goes here.
 
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { getJobList } from './api/jobs'
 
+getJobList().then((data)=>{
+    console.log(data);
+})
