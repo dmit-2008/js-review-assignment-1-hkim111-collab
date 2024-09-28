ae58ab8dca664f5aa5f8593f13808d2505ab5947
Hyeonseo <tym30003@gmail.com>
2024-09-28 16:52:39 -0600

    Update 3

--- frontend/js/api/jobs.js
+++ frontend/js/api/jobs.js
@@ -10,4 +10,21 @@ const getJobList=(url=`${BASE_URL}/jobs/`)=>{
     })
 }
 
+const renderJob=(jobData,listElement)=>{
+    listElement.innerHTML+=`<li class="job-card card my-1" style="width: 18rem;">
+    <div class="card-header">${jobData.company}</div>
+    <div class="card-body">
+      <h5 class="card-title">${jobData.title}</h5>
+      <h6 class="card-subtitle mb-2 text-body-secondary">${jobData.location}</h6>
+      <h6 class="card-subtitle mb-2 text-body-secondary">Posted ${jobData.date_posted}</h6>
+      <button class="btn btn-primary view-job-button" job-data-id="${jobData.id}">View Job</button>
+    </div>
+  </li>`
+}
+
+const searchButton = document.getElementById("search-button");
+
+searchButton.addEventListener
+
 export {getJobList}
+export {renderJob}
--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,8 +1,19 @@
 // your code goes here.
 
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { getJobList } from './api/jobs'
+import { getJobList } from './api/jobs.js'
+import { renderJob } from './api/jobs.js';
+
+const jobList=document.querySelector("#searched-jobs");
 
 getJobList().then((data)=>{
     console.log(data);
+    if (data) {
+        data.forEach((jobData) => {
+            renderJob(jobData, jobList);
+        });
+    } else {
+        jobList.innerHTML+=`<div class="text-dark">No Results Found</div>`;
+    }
 })
+
